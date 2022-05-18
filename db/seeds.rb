f9e98baaf539d7285b56b27d6526cf14f658c8cd
Maxime ANDRE <andremaxime7@gmail.com>
2022-05-18 10:55:35 +0200

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Superhero.name,
+    content: Faker::Hacker.say_something_smart
+  )
+  article.save!
+end
